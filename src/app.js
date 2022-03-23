14850586af07faa7ec3fa2fd40007546707c2093
Gaston Ortega <gasortega@gmail.com>
2022-03-22 23:33:25 -0300

    cleaning old routes

--- src/app.js
+++ src/app.js
@@ -11,8 +11,3 @@ app.use('/',rutas);
 
 app.listen(port, ()=> console.log("server "+port+" ok"));
 
-//app.get("/", (req, res)=> res.sendFile(path.join(__dirname, "views", "index.html")));
-//app.get("/productDetail.html", (req, res)=> res.sendFile(path.join(__dirname, "views", "productDetail.html")));
-//app.get("/productCart.html", (req, res)=> res.sendFile(path.join(__dirname, "views", "productCart.html")));
-//app.get("/register.html", (req, res)=> res.sendFile(path.join(__dirname, "views", "register.html")));
-//app.get("/login.html", (req, res)=> res.sendFile(path.join(__dirname, "views", "login.html")));
