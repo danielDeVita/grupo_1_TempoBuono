6f188e6e18a555cf657efb01ef08252028af39c0
Gaston Ortega <gasortega@gmail.com>
2022-03-24 22:10:58 -0300

    [WIP] Routes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ const port = process.env.PORT || 3000;
 
 const routes = require(path.join(__dirname,'.','routes','routes.js'));
 const routesUser = require(path.join(__dirname,'.','routes','routesUser.js'));
-const routesProduct = require(path.join(__dirname,'.','routes','routesUser.js'));
+const routesProduct = require(path.join(__dirname,'.','routes','routesProduct.js'));
 
 app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views","./views")
+
+
 app.use('/',routes);
 app.use('/',routesUser);
 app.use('/',routesProduct);
