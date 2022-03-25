e4eb57d43fff26dd724c872e01c61116ae5ca88b
danielDeVita <danielDeVita>
2022-03-25 01:15:20 -0300

    rutas ok

--- src/routes/mainRoutes.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-let mainController = require('../controllers/mainController.js');
-
-router.get('/', mainController.index);
-
-module.exports = router;
--- src/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-const path = require("path");
-let productController = require(path.join(__dirname,'../controllers/productController.js'));
-
-router.get('/detail', productController.productDetail);
-router.get('/cart', productController.productCart);
-
-module.exports = router;
