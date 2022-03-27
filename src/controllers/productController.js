15fdae145e07b680641be5ef8b8793ca5947bf33
danielDeVita <danielDeVita>
2022-03-27 19:41:02 -0300

    lista de productos dinamica

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,40 +1,40 @@
 
 
 const productController = {
-    productCart: (req, res) =>{
+    productCart: (req, res) => {
         res.render('productCart');
     },
 
-    productDetail: (req, res) =>{
+    productDetail: (req, res) => {
         res.render('productDetail');
     },
-    
-    productList: (req, res)=> {
+
+    productList: (req, res) => {
 
         let productos = [
             {
-                titulo: "Alfajor Mar Del Plata" ,
+                titulo: "Alfajor Mar Del Plata",
                 descripcion: "Dos galletitas rellenas del mas delicioso Dulce de Leche, cubiertas con chocolate semi-amargo de máxima pureza.",
                 precio: "USD 50",
                 categoria: "Alfajores",
-                imagen: ""
+                imagen: "/img/img_alfajor_marplatense.png"
             },
             {
-                titulo: "Café Sierra" ,
+                titulo: "Café Sierra",
                 descripcion: "Un cafê con gratas notas aromáticas dulces y afrutadas que recuerdan el caramelo y un suave chocolate oscuro, de máxima pureza.",
                 precio: "USD 300",
                 categoria: "Cafés",
-                imagen: ""
+                imagen: "/img/img_alfajor_marplatense.png"
             },
             {
-                titulo: "Sierra Marplatense" ,
+                titulo: "Sierra Marplatense",
                 descripcion: "Una combinación perfecta que logra reslatar el aroma del café con el toque dulce del alfajor, de máxima pureza.",
                 precio: "USD 325",
                 categoria: "Combos",
-                imagen: ""
+                imagen: "/img/img_alfajor_marplatense.png"
             }
         ]
-        res.render('productList', {productos})
+        res.render('productList', {productos:productos})
     }
 }
 
