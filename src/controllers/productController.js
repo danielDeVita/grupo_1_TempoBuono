d33c5890439565601b181858014019837e45b146
Maga805 <maleja8005@gmail.com>
2022-03-27 16:50:22 -0500

    lista productos (artículos) html y css

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,7 +10,31 @@ const productController = {
     },
     
     productList: (req, res)=> {
-        res.render('productList')
+
+        let productos = [
+            {
+                titulo: "Alfajor Mar Del Plata" ,
+                descripcion: "Dos galletitas rellenas del mas delicioso Dulce de Leche, cubiertas con chocolate semi-amargo de máxima pureza.",
+                precio: "USD 50",
+                categoria: "Alfajores",
+                imagen: ""
+            },
+            {
+                titulo: "Café Sierra" ,
+                descripcion: "Un cafê con gratas notas aromáticas dulces y afrutadas que recuerdan el caramelo y un suave chocolate oscuro, de máxima pureza.",
+                precio: "USD 300",
+                categoria: "Cafés",
+                imagen: ""
+            },
+            {
+                titulo: "Sierra Marplatense" ,
+                descripcion: "Una combinación perfecta que logra reslatar el aroma del café con el toque dulce del alfajor, de máxima pureza.",
+                precio: "USD 325",
+                categoria: "Combos",
+                imagen: ""
+            }
+        ]
+        res.render('productList', {productos})
     }
 }
 
