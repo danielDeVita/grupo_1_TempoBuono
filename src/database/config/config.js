b252042b105dc15b3c4560f11201f49088f812fb
danielDeVita <danielDeVita>
2022-05-28 20:44:06 -0300

    paquetes instalados y modelos

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.export = {
+  "development": {
+    "username": "root",
+    "password": "root",
+    "database": "tempo_buono",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port": "8889"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ src/database/models/users.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Users";
+    let cols = {
+        idUsers: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        UsersNombre: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+        UsersEmail: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+        UsersPasswd: {
+            type: dataTypes.STRING(255),
+            allowNull: false
+        },
+        usersCategory_idusersCategory: {
+            type: dataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false
+        },
+    };
+    let config = {
+        tableName: "users",
+        timestamps: true
+    };
+    const Users = sequelize.define(alias, cols, config);
+    //aca van las relaciones Users.associate
+    return Users;
+}
