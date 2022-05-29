7ec9d0b34bce197bd6f17f5529443a72100ebcc4
Maga805 <maleja8005@gmail.com>
2022-05-29 18:23:20 -0500

    asociaciones entre cart y product con tabla pivot

--- src/database/models/cart.js
+++ src/database/models/cart.js
@@ -1,35 +1,42 @@
 module.exports = (sequelize, dataTypes) => {
-    let alias = "Cart";
-    let cols = {
-        idCart: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        CardQantity: {
-            type: dataTypes.INTEGER,
-            allowNull: false
-        },
-        CartTotal: {
-            type: dataTypes.FLOAT,
-            allowNull: false
-        },
-        CartDate: {
-            type: dataTypes.DATE,
-            allowNull: false
-        },
-    };
-    let config = {
-        tableName: "Cart",
-        timestamps: true
-    };
-    const Cart = sequelize.define(alias, cols, config);
-    Cart.associate = function (models) {
-        Cart.hasMany(models.users, {
-            as: "users",
-            foreignKey: "idCart"
-        })
-    }
-    return Cart;
-}
+  let alias = "Cart";
+  let cols = {
+    idCart: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    CardQantity: {
+      type: dataTypes.INTEGER,
+      allowNull: false,
+    },
+    CartTotal: {
+      type: dataTypes.FLOAT,
+      allowNull: false,
+    },
+    CartDate: {
+      type: dataTypes.DATE,
+      allowNull: false,
+    },
+  };
+  let config = {
+    tableName: "Cart",
+    timestamps: true,
+  };
+  const Cart = sequelize.define(alias, cols, config);
+  Cart.associate = function (models) {
+    Cart.hasMany(models.users, {
+      as: "users",
+      foreignKey: "idCart",
+    }),
+      Cart.belongsToMany(models.products, {
+        as: "products", //otra posibilidad es Cart (tabla de la relación)
+        through: "Cart_has_products",
+        foreignKey: "idCart",
+        otherKey: "idProd",
+        timestamps: false,
+      });
+  };
+  return Cart;
+};
--- src/database/models/products.js
+++ src/database/models/products.js
@@ -1,30 +1,38 @@
 module.exports = (sequelize, dataTypes) => {
-    let alias = "products";
-    let cols = {
-        idProd: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-        ProductsName: {
-            type: dataTypes.STRING(45),
-            allowNull: false
-        },
-        ProductsDescription: {
-            type: dataTypes.STRING(45),
-            allowNull: false
-        },
-        ProductsPrice: {
-            type: dataTypes.FLOAT(255),
-            allowNull: false
-        },
-    };
-    let config = {
-        tableName: "products",
-        timestamps: true
-    };
-    const products = sequelize.define(alias, cols, config);
-    //products.associate = function(models){}
-    return products;
-}
+  let alias = "products";
+  let cols = {
+    idProd: {
+      type: dataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    ProductsName: {
+      type: dataTypes.STRING(45),
+      allowNull: false,
+    },
+    ProductsDescription: {
+      type: dataTypes.STRING(45),
+      allowNull: false,
+    },
+    ProductsPrice: {
+      type: dataTypes.FLOAT(255),
+      allowNull: false,
+    },
+  };
+  let config = {
+    tableName: "products",
+    timestamps: true,
+  };
+  const products = sequelize.define(alias, cols, config);
+  products.associate = function (models) {
+    products.belongsToMany(models.cart, {
+      as: "cart", //otra posibilidad es products (tabla de la relación)
+      through: "Cart_has_products",
+      foreignKey: "idProd",
+      otherKey: "idCart",
+      timestamps: false,
+    });
+  };
+  return products;
+};
