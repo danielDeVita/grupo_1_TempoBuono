425e0f218e1194298b117fbbda2543537e7c5319
danielDeVita <danielDeVita>
2022-06-04 20:51:08 -0300

    falta updatear las imagenes y hacer el metodo destroy

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,12 +1,6 @@
-//const { render,redirect } = require('express/lib/response');
 const fs = require("fs");
 const path = require("path");
-/* let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf-8')); */
 const db = require("../database/models");
-
-/* function productsShowTrue(){
-return products = products.filter(product => product.show ==true);
-} */
 
 const productController = {
   productCart: (req, res) => {
@@ -17,7 +11,6 @@ const productController = {
   },
 
   productDetail: (req, res) => {
-    //ok, falta solo agregar imagenes en la DB
     db.products
       .findByPk(req.params.idProd, {
         include: ["productsImages", "productsCategory"],
@@ -29,12 +22,9 @@ const productController = {
           user: req.session.usuarioLogueado,
         });
       });
-    //let producto = products.find(producto=>producto.id==req.params.id)
-    //return res.render('productDetail', {producto, styles: 'productDetail', user: req.session.usuarioLogueado});
   },
 
   productList: (req, res) => {
-    //ok, falta solo agregar imagenes en la DB
     db.products
       .findAll({
         include: ["productsImages"],
@@ -46,8 +36,6 @@ const productController = {
           user: req.session.usuarioLogueado,
         });
       });
-    /* let products = productsShowTrue();
-        res.render('productList', {products, styles: "productList", user: req.session.usuarioLogueado}); */
   },
 
   crearProductoForm: (req, res) => {
@@ -58,42 +46,29 @@ const productController = {
   },
 
   crearProducto: (req, res) => {
-    db.products
-      // let arrayProductos = [...products]
-      .create({
-        // id: arrayProductos.length > 0 ? arrayProductos[arrayProductos.length -1].id + 1 : 1,
-        ProductsName: req.body.nombre_producto,
-        ProductsDescription: req.body.descripcion_producto,
-        ProductsPrice: req.body.precio_producto,
-        productsCategory_idproductsCategory: req.body.categoria // El valor 1,2 o 3, por select/option
-        //productsImagesNombre: req.file?.filename ?? "default-image.png",
-        // show:true
-      })
-      //Asesoria con Fede
-      .then((product) => { //product es el producto creado en db
+    db.products.create({
+      ProductsName: req.body.nombre_producto,
+      ProductsDescription: req.body.descripcion_producto,
+      ProductsPrice: req.body.precio_producto,
+      productsCategory_idproductsCategory: req.body.categoria
+    })
+      .then((product) => {
         db.productsImages.create({
           productsImagesNombre: req.file?.filename ?? "default image coffee.png",
           productsImagesDesc: "",
           products_idProd: product.idProd
         })
-      .then(()=>{
-          return res.redirect("/products");
-        })
-      // .then((image)=>{
-      //   // image.setProductsImages(product)
-      //   return res.redirect("/products");
-      // })
+          .then(() => {
+            return res.redirect("/products");
+          })
       })
       .catch((error) => console.error(error));
-    // arrayProductos.push(newProduct);
-    // fs.writeFileSync(path.join(__dirname, '../data/products.json'), (JSON.stringify(arrayProductos, null, 2)));
   },
 
   modProductoForm: (req, res) => {
-    db.products
-      .findByPk(req.params.idProd, {
-        include: ["productsImages", "productsCategory"],
-      })
+    db.products.findByPk(req.params.idProd, {
+      include: ["productsImages", "productsCategory"],
+    })
       .then((producto) => {
         return res.render("modificar", {
           producto,
@@ -101,75 +76,42 @@ const productController = {
           user: req.session.usuarioLogueado,
         });
       });
-
-    // if (product) {
-    //     res.render('modificar', { styles: 'crearProducto', product, user: req.session.usuarioLogueado });
-    // }
-    // else {
-    //     res.send('404 Not Found ');
-    // }
   },
 
   modProducto: (req, res) => {
-    // let arrayProducts = [...products]
-    db.products
-      .update(
-        {
-          ProductsName: req.body.nombre_producto,
-          ProductsDescription: req.body.descripcion_producto,
-          ProductsPrice: req.body.precio_producto,
-          productsCategory_idproductsCategory: req.body.categoria,
-          // productsImagesNombre: req.file?.filename ?? "default-image.png",
-        }, //Preguntar a fede como subir imagenes a DB.
-
-        {
-          where: { idProd: req.params.idProd }, //Comparación id modelo con id URL.
-        }
-      )
-      .then((product) => { //product es el producto creado en db
+    db.products.update(
+      {
+        ProductsName: req.body.nombre_producto,
+        ProductsDescription: req.body.descripcion_producto,
+        ProductsPrice: req.body.precio_producto,
+        productsCategory_idproductsCategory: req.body.categoria,
+      },
+      {
+        where: { idProd: req.params.idProd },
+      }
+    )
+      .then((product) => {
         db.productsImages.update({
           productsImagesNombre: req.file?.filename ?? "default image coffee.png",
           productsImagesDesc: "",
           products_idProd: product.idProd
         },
-        // {where: { idProd: req.params.idProd }}
         )
-      .then(()=>{
-          return res.redirect("/products");
-        })
+          .then(() => {
+            return res.redirect("/products");
+          })
       })
 
       .catch((error) => console.error(error));
-
-    // let modProduct = products.map(product => {
-    //     if (product.id == req.params.id) {
-    //         product.name = req.body.nombre_producto,
-    //             product.price = req.body.precio_producto,
-    //             product.category = req.body.categoria,
-    //             product.description = req.body.descripcion_producto,
-    //             product.image = req.file?.filename ?? "default-image.png"
-    //     }
-    //     return product
-    // });
-
-    // fs.writeFileSync(path.join(__dirname, '../data/products.json'), (JSON.stringify(modProduct, null, 2)));
-    // return res.redirect('/products');
   },
 
   deleteProducto: (req, res) => {
-    const id = req.params.id;
-    products = products.map((product) => {
-      if (product.id == id) {
-        product.show = false;
-      }
-      return product; //producto a borrar
-    });
-
-    fs.writeFileSync(
-      path.join(__dirname, "../data/products.json"),
-      JSON.stringify(products, null, 2)
-    );
-    return res.redirect("/products");
+    let id = req.params.idProd;
+    db.products.destroy({ where: { idProd: id }, force: true })
+      .then(() => {
+        return res.redirect("/products");
+      })
+      .catch(err => console.error(err))
   },
 };
 
