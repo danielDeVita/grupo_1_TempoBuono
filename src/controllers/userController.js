7bc861b602b0ed27d031d0dc3424a33288a65740
danielDeVita <danielDeVita>
2022-06-06 20:20:39 -0300

    cleanup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,6 @@
-//const fs = require("fs");
 const path = require("path");
-//const usuarios = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8'));
 const { validationResult } = require('express-validator');
 const bcryptjs = require('bcryptjs');
-//const req = require("express/lib/request");
 const db = require("../database/models")
 
 const userController = {
@@ -24,24 +21,11 @@ const userController = {
                 UsersImageName: req.file?.filename ?? "default-user-image.png",
                 usersCategory_idusersCategory: req.body.categoria
             })
-            .then(user=>{
-                return res.redirect('/login');
-            })
-            .catch(err=>console.error(err))
-
-            /* let arrayUsuarios = [...usuarios] */
-
-            /* let newUser = {
-                id: arrayUsuarios.length > 0 ? arrayUsuarios[arrayUsuarios.length - 1].id + 1 : 1,
-                usuario: req.body.usuario,
-                email: req.body.email,
-                password: bcryptjs.hashSync(req.body.password, 12),
-                image: req.file?.filename ?? "default-user-image.png"
-            } */
-            /* arrayUsuarios.push(newUser); */
-            /* fs.writeFileSync(path.join(__dirname, '../data/users.json'), (JSON.stringify(arrayUsuarios, null, 2))); */
+                .then(user => {
+                    return res.redirect('/login');
+                })
+                .catch(err => console.error(err))
         }
-
     },
     login: (req, res) => {
         res.render('login', { styles: 'login' })
@@ -51,33 +35,28 @@ const userController = {
 
         if (errors.isEmpty()) {
             let usuarioALoguearse
-            //hacer un find all de db.Users
             db.Users.findAll()
-            .then(usuarios=>{
-                
-                for (let i = 0; i < usuarios.length; i++) {
-                    if ((usuarios[i].UsersEmail == req.body.email) && (bcryptjs.compareSync(req.body.password, usuarios[i].UsersPasswd))) {
-                        usuarioALoguearse = usuarios[i];
-                        req.session.usuarioLogueado = usuarioALoguearse
-    
-                        if (req.body.keepLogin) {
-                            res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 2 })
-                        }
-    
-                        return res.redirect("/profile")
-                    }
-    
-                }
-                if (usuarioALoguearse == undefined) {
-                    res.render('login', { styles: 'login', errors: [{ msg: "Credenciales inválidas" }] })
-                }
-             else {
-                res.render('login', { styles: 'login', errors: errors.mapped() })
-            }
-            })//llave y parentesis del THEN
+                .then(usuarios => {
 
-    }},
-    
+                    for (let i = 0; i < usuarios.length; i++) {
+                        if ((usuarios[i].UsersEmail == req.body.email) && (bcryptjs.compareSync(req.body.password, usuarios[i].UsersPasswd))) {
+                            usuarioALoguearse = usuarios[i];
+                            req.session.usuarioLogueado = usuarioALoguearse
+                            if (req.body.keepLogin) {
+                                res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 2 })
+                            }
+                            return res.redirect("/profile")
+                        }
+                    }
+                    if (usuarioALoguearse == undefined) {
+                        res.render('login', { styles: 'login', errors: [{ msg: "Credenciales inválidas" }] })
+                    }
+                    else {
+                        res.render('login', { styles: 'login', errors: errors.mapped() })
+                    }
+                })
+        }
+    },
     profile: (req, res) => {
         res.render("profile", { styles: "profile", user: req.session.usuarioLogueado });
     },
@@ -87,12 +66,6 @@ const userController = {
         req.session.destroy()
         return res.redirect('/')
     },
-    list: (req, res) => {
-        db.Users.findAll()
-            .then(users => {
-                res.send(users)
-            })
-    }
 }
 
 module.exports = userController
