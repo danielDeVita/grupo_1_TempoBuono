7ea5e2e29bcbd3769d237cbe0f1d9789d0110e1a
danielDeVita <danielDeVita>
2022-06-08 01:24:12 -0300

    user logged middleware arreglado para que use la DB

--- src/middleware/userLoggedMiddleware.js
+++ src/middleware/userLoggedMiddleware.js
@@ -1,30 +1,36 @@
-const fs = require("fs");
+/* const fs = require("fs");
 const path = require("path");
 const usuarios = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf-8'));
-
+ */
+const db = require("../database/models")
 function userLoggedMiddleware(req, res, next) {
     res.locals.isLogged = false;
     let emailInCookie = req.cookies.userEmail;
     let userFromCookie
 
-    for (let i = 0; i < usuarios.length; i++) {
+    db.Users.findAll()
+    .then(usuarios =>{
 
-        if (usuarios[i].email == emailInCookie) {
-            userFromCookie = usuarios[i]
+        for (let i = 0; i < usuarios.length; i++) {
+    
+            if (usuarios[i].UsersEmail == emailInCookie) {
+                userFromCookie = usuarios[i]
+            }
         }
-    }
+    
+        if (userFromCookie) {
+            req.session.usuarioLogueado = userFromCookie;
+        }
+    
+        if (req.session && req.session.usuarioLogueado) {
+    
+            res.locals.isLogged = true;
+            res.locals.usuarioLogueado = req.session.usuarioLogueado;
+        }
+    
+        next();
+    })
 
-    if (userFromCookie) {
-        req.session.usuarioLogueado = userFromCookie;
-    }
-
-    if (req.session && req.session.usuarioLogueado) {
-
-        res.locals.isLogged = true;
-        res.locals.usuarioLogueado = req.session.usuarioLogueado;
-    }
-
-    next();
 }
 
 module.exports = userLoggedMiddleware;
