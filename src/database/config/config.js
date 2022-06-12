e9fddd5788ea78bdf2684a2e80caae9f7f05ab3c
FederDiaz <federico.r.diaz@hotmail.com>
2022-06-12 12:33:50 -0300

    agregando links a la lista de productos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,13 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "root",
+    // "password": "",
     "database": "Tempo_Buono_New",
+    // "database": "tempo_buono_new",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port": "8889"
+    // "port": "3306"
   },
   "test": {
     "username": "root",
