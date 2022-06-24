e2c537f67b1de44881461dbc0043bc6c8b709944
danielDeVita <danielitodevita@gmail.com>
2022-06-24 19:29:25 -0300

    register front end validation OK

--- public/js/register-val.js
+++ public/js/register-val.js
@@ -12,10 +12,10 @@ window.onload = function() {
     let usuario = document.getElementById('usuario')
     console.log(typeof usuario.value)
     console.log(usuario.value)
-    if (!usuario.value || usuario.value === ""){//al dar TRUE jamas pudimos hacer que vuelva el estilo "is-invalid"
+    if (!usuario.value || usuario.value === ""){
       errores + 1;
       
-      usuario.nextElementSibling.classList.add("is-invalid"); // HACER EN TODOS LOS CAMPOS
+      usuario.nextElementSibling.classList.add("is-invalid");
       usuario.nextElementSibling.classList.remove("is-valid");
       usuario.nextElementSibling.innerHTML = "El campo no puede estar vacío";
     } else if (usuario.value.length < 2) {
@@ -29,12 +29,14 @@ window.onload = function() {
   })
 
   imagenUsuario.addEventListener("blur", function (e) {
-    if (!imagenUsuario.value) { //no sabemos bien como validar una imagen, es con un ".include(arrayExtensiones)"?? Y //al dar TRUE jamas pudimos hacer que vuelva el estilo "is-invalid"
+    if (!imagenUsuario.value) { 
         errores + 1;
+
         imagenUsuario.nextElementSibling.classList.add("is-invalid");
+        imagenUsuario.nextElementSibling.classList.remove("is-valid");
         imagenUsuario.nextElementSibling.innerHTML = "Debes subir una imagen";
     } else {
-        imagenUsuario.nextElementSibling.classList.toggle("is-valid");
+        imagenUsuario.nextElementSibling.classList.replace("is-invalid", "is-valid");
     }
   })
 
@@ -46,27 +48,30 @@ window.onload = function() {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 }; */
-    if(!email.value || email.value === ""){//al dar TRUE jamas pudimos hacer que vuelva el estilo "is-invalid"
+    if(!email.value || email.value === ""){
       errores + 1;
       email.nextElementSibling.classList.add("is-invalid");
+      email.nextElementSibling.classList.remove("is-valid");
       email.nextElementSibling.innerHTML= "Ingresa tu email";
     } else { // Preguntar a Fede cómo se valida un email. @ (metodo "find/include/indexOf"? si es que es un array...)
-      email.nextElementSibling.classList.toggle("is-valid");
+      email.nextElementSibling.classList.replace("is-invalid", "is-valid");
     }
   })
 
   password.addEventListener('blur', function(e){
 
-    if (!password.value || password.value === ""){//al dar TRUE jamas pudimos hacer que vuelva el estilo "is-invalid"
+    if (!password.value || password.value === ""){
       errores + 1;
       password.nextElementSibling.classList.add("is-invalid");
+      password.nextElementSibling.classList.remove("is-valid");
       password.nextElementSibling.innerHTML = "El campo no puede estar vacío";
     } else if (password.value.length < 8) {
       errores + 1;
       password.nextElementSibling.classList.add("is-invalid");
+      password.nextElementSibling.classList.remove("is-valid");
       password.nextElementSibling.innerHTML = "Por favor digita al menos ocho caracteres"
     } else {
-      password.nextElementSibling.classList.toggle("is-valid");
+      password.nextElementSibling.classList.replace("is-invalid", "is-valid");
     }
   })
 
