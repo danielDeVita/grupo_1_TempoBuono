6e39253217f128e2a363b313c79a77779ff10512
danielDeVita <danielitodevita@gmail.com>
2022-07-07 21:51:23 -0300

    fetchs y useEffect

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -3,15 +3,32 @@ import TotalCard from './Components/TotalCard'
 import LastProduct from './Components/LastProduct'
 import ProductsByCat from './Components/ProductsByCat'
 import ProductList from './Components/ProductList'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/users")
+      .then(response => response.json())
+      .then(data => {
+        setUsers(data.users)
+      })
+    fetch("http://localhost:3001/api/products")
+      .then(response => response.json())
+      .then(data => {
+        setProducts(data.products)
+      })
+  }, [])
 
   return (
     <div className="App">
       <TotalCard />
       <LastProduct />
-      <ProductsByCat/>
-      <ProductList/>
+      <ProductsByCat />
+      <ProductList />
     </div>
   );
 }
