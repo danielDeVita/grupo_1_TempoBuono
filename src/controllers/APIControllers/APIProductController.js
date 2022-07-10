fff5af94bc2c1de079a9f5dd75eefdfebea119dc
danielDeVita <danielitodevita@gmail.com>
2022-07-10 20:04:44 -0300

    pruebas para ver la urlImage

--- src/controllers/APIControllers/APIProductController.js
+++ src/controllers/APIControllers/APIProductController.js
@@ -2,41 +2,44 @@ const path = require("path");
 const db = require("../../database/models");
 
 const APIProductController = {
-    list: (req, res) => {
-      db.products.findAll({include: ["productsCategory"]}).then((products) => {
-        let respuesta = {
-          meta: {
-            status: 200,
-            count: products.length,
-          },
-          products: {
-            products,
-            detail: /* products.map(product, index)=>{} */"api/products/:idProd", //cómo se resuelve? (hacer un map del array productos y meterle el detail)
-          },
-        };
-        res.json(respuesta);
-      });
-    },
+  list: (req, res) => {
+    db.products.findAll({ include: ["productsCategory"] }).then((products) => {
+      let respuesta = {
+        meta: {
+          status: 200,
+          count: products.length,
+        },
+        products: {
+          products,
+          detail: /* products.map(product, index)=>{} */"api/products/:idProd", //cómo se resuelve? (hacer un map del array productos y meterle el detail)
+        },
+      };
+      res.json(respuesta);
+    });
+  },
 
-    detailProduct: (req, res) => {
-        db.products.findByPk(req.params.idProd).then((product) => { //hacer un include con category
-          let respuesta = {
-            meta: {
-              status: 200,
-            },
-            data: { //nos falta también un array con principal relación de uno a muchos
-                id: product.idProd,
-                name: product.ProductsName,
-                description: product.ProductsDescription,
-                price: product.ProductsPrice,
-                urlImage: "No sabemos" // Estamos investigando como resolverlo. (map del array)
-            }
-          };
-          res.json(respuesta);
-        });
-      },
+  detailProduct: (req, res) => {
 
+    //let prueba = db.sequelize.query(`SELECT products.idProd, productsImages.productsImagesNombre FROM products INNER JOIN productsImages ON products.idProd = productsImages.products_idProd`, { type: db.sequelize.QueryTypes.SELECT })
 
+    db.products.findByPk(req.params.idProd).then((product) => { //hacer un include con category
+      let respuesta = {
+        meta: {
+          status: 200,
+        },
+        data: { //nos falta también un array con principal relación de uno a muchos
+          id: product.idProd,
+          name: product.ProductsName,
+          description: product.ProductsDescription,
+          price: product.ProductsPrice,
+          urlImage: "No sabemos"  // Estamos investigando como resolverlo. (map del array) 
+        }
+      };
+      res.json(respuesta);
+
+    });
+  },
 }
 
 module.exports = APIProductController;
+
