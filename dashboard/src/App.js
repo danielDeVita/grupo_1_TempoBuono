2778732c3c539fc5eb3e4c1439fdda523507da35
danielDeVita <danielitodevita@gmail.com>
2022-07-10 23:42:32 -0300

    casi casi resuelto lastProduct

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -7,44 +7,44 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [data, setData] = useState({users:{}, products:[]});
+  const [data, setData] = useState({ users: {}, products: [] });
 
-  async function peticiones(){
+  async function peticiones() {
 
     let urls = [
       "http://localhost:3001/api/users",
       "http://localhost:3001/api/products"
     ]
-    
+
     const arrayPromesas = []
-  
-    for(let i= 0; i< urls.length; i++){
+
+    for (let i = 0; i < urls.length; i++) {
       const dataFetch = await fetch(urls[i])
       const data = await dataFetch.json()
       arrayPromesas.push(data)
     }
 
     const result = await Promise.all(arrayPromesas)
-    setData({users:result[0].data, products:result[1].products.products})
+    setData({ users: result[0].data, products: result[1].products.products })
   }
 
   useEffect(
-    ()=>{
+    () => {
       peticiones()
-    },[]
+    }, []
   )
 
-   console.log (data.users)
-   console.log (data.products)
+  console.log(data.users)
+  console.log(data.products)
 
-   const lastProduct = data.products.pop()
+  //const lastProduct = data.products.pop() este metodo no nos sirve porque elimina un producto que deberia viajar a <ProductList/>
 
   return (
     <div className="App">
-      <TotalCard users = {data.users.count} products = {data.products} />
-      <LastProduct lastProduct = {lastProduct} />
+      <TotalCard users={data.users.count} products={data.products} />
+      <LastProduct products={data.products} />
       <ProductsByCat />
-      <ProductList products = {data.products}/>
+      <ProductList products={data.products} />
     </div>
   );
 }
--- dashboard/src/components/LastProduct.js
+++ dashboard/src/components/LastProduct.js
@@ -2,12 +2,17 @@ import React from 'react';
 
 function LastProduct(props) {
 
+    let lastProduct = props.products[props.products.length - 1] 
+    console.log(lastProduct)
+
+
     return (
         <div className="card" style={{ width: '18rem' }}>
             <img src="..." className="card-img-top" alt="foto-producto" />
             <div className="card-body">
-                <h5 className="card-title">Ultimo producto creado</h5>
-                <p className="card-text">{props.lastProduct.ProductsName}</p>
+               {/*  <h5 className="card-title">{lastProduct.ProductsName}</h5>
+                <p className="card-text">{lastProduct.ProductsDescription}</p>
+                <p className="card-text">$ {lastProduct.ProductsPrice}</p> */}
             </div>
         </div>
     )
