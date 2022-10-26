157a88a677c3ed8222ea8d1cbf3257cba91e3219
danielDeVita <danielitodevita@gmail.com>
2022-10-26 10:36:48 -0300

    back to start

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-console.log(process.env)
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT 
+const port = process.env.PORT || 3001;
 const methodOverride = require("method-override");
 const session = require("express-session")
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
+require('dotenv').config();
+console.log(process.env)
 
 const routes = require(path.join(__dirname, '.', 'routes', 'routesMain.js'));
 const routesUser = require(path.join(__dirname, '.', 'routes', 'routesUser.js'));
