45b4ad31049347edf241c6f04db4c0f014a8e4d7
danielDeVita <danielitodevita@gmail.com>
2022-10-26 00:25:05 -0300

    changed port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3001;
+const port = /* process.env.PORT ||  */3001;
 const methodOverride = require("method-override");
 const session = require("express-session") 
 const cookieParser = require("cookie-parser");
