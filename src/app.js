1effcc3c48debf35c4ec4e3ee96a06bf69dc0be3
danielDeVita <danielitodevita@gmail.com>
2022-10-26 10:15:13 -0300

    new port for railway

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.PORT || 3001;
+//const port = process.env.PORT || 3001;
+const port = process.env.IS_RAILWAY ? '0.0.0.0:3333' : 3333;
 const methodOverride = require("method-override");
 const session = require("express-session")
 const cookieParser = require("cookie-parser");
@@ -44,4 +45,5 @@ app.use("/api/categories", APIRouterCategories);
 
 app.use((req, res, next) => res.status(404).render("404"));
 
-app.listen(port, () => console.log("server " + port + " ok"));
+//app.listen(port, () => console.log("server " + port + " ok"));
+app.listen(port, () => console.log(`Server runnning on port ${port}!`));
