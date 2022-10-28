c90e3e23cf2eb4048d5b7393f82d3c0c682b56f7
danielDeVita <danielitodevita@gmail.com>
2022-10-28 12:26:33 -0300

    no PORT variable coded

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const path = require("path");
-const PORT = process.env.PORT
+//const PORT = process.env.PORT
 const methodOverride = require("method-override");
 const session = require("express-session")
 const cookieParser = require("cookie-parser");
